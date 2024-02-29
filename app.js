51cc7b899ceb1c8d72867f2a419a2a83d944b7b3
jsomaiya21 <js212@rice.edu>
2024-02-29 11:56:18 -0600

    port variable

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
+const port = 3001
 
 //middleware
 app.use(express.json());
 
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3001");
 });
 
 module.exports = app;
-

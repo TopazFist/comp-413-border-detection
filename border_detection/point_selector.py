8569c730d066e478d501072eb9b1c3c98d58883f
Atishay Jain <atishayericjain@gmail.com>
2024-02-28 19:52:02 -0600

    trying hyperparams

--- border_detection/point_selector.py
+++ border_detection/point_selector.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import cv2
 from scipy.signal import argrelextrema
 import numpy as np
-
+import math
 
 def histogram_creater(image):
     img = cv2.imread(image, cv2.COLOR_BGR2GRAY)
@@ -18,7 +18,7 @@ def choose_pixel(image):
     # image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     histg = cv2.calcHist([image],[0],None,[256],[0,256])
-    histg2 = histg[:90]
+    histg2 = histg[:95]
 
     max_val = float("-inf")
     color_val = -1
@@ -31,12 +31,20 @@ def choose_pixel(image):
     img_x, img_y = 0, 0
 
 
+    dist = float("inf")
+
+    w1 = 0.85
+    w2 = 0.15
+
+
+
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             # print(image[i,j])
-            if image[i,j] == color_val:
+            if int(color_val) <= int(image[i,j]) and math.sqrt(((i-(image.shape[0]//2))**2) + ((j-(image.shape[1]//2))**2)) < dist:
+                dist = math.sqrt(((i-image.shape[0])**2) + ((j-image.shape[1])**2))
                 img_x, img_y = i, j
-                return img_x, img_y
+    print(img_x, img_y)
     return img_x, img_y
 
 # c, d = choose_pixel("ISIC_5341087.JPG")

6119964f189edbb62366fb2e0770f672b9a7b5bd
Atishay Jain <atishayericjain@gmail.com>
2024-03-22 16:40:06 -0500

    Evaluation metrics added

--- border_detection/evaluation.py
+++ border_detection/evaluation.py
@@ -22,41 +22,52 @@ def dice(image1, image2):
 
 def main():
     ground_truth_folder = "ISIC-images/ISBI2016_ISIC_Part1_Test_GroundTruth"
-    testing_images_folder = "ISIC-images/ISBI2016_ISIC_Part1_Test_Data"
-    filenames = os.listdir(testing_images_folder)
-    file_paths = [os.path.join(testing_images_folder, filename) for filename in filenames]
+    testing_inputs = "ISIC-images/ISBI2016_ISIC_Part1_Test_Data"
+    algorithm_output = "ISIC-testing-output"
 
-    file_paths = file_paths[:10]
-    print(file_paths)
+    filenames = os.listdir(testing_inputs)
+    file_paths = [os.path.join(testing_inputs, filename) for filename in filenames]
+    file_paths = file_paths[:40]
 
     max_threads = 8
     with concurrent.futures.ProcessPoolExecutor(max_workers=max_threads) as executor:
-        futures = [executor.submit(process_image, file_path, "ISIC-testing-images/testing_data_ISIC", False) for file_path in file_paths]
+        futures = [executor.submit(process_image, file_path, algorithm_output, False) for file_path in file_paths]
         concurrent.futures.wait(futures)
     
-    filenames_testing = os.listdir("ISIC-testing-images/testing_data_ISIC")
-    file_paths_testing = [os.path.join(testing_images_folder, filename) for filename in filenames_testing]
 
-    file_paths_testing = file_paths[:10]
+    filenames_testing_output_files = os.listdir(algorithm_output)
+    output_filepaths = [os.path.join(algorithm_output, filename) for filename in filenames_testing_output_files]
+    print(output_filepaths)
 
-    for base_file_path in file_paths_testing:
-        base_filename = os.path.basename(base_file_path)
-        segmentation_filename = base_filename.replace(".", "_Segmentation.")
-        segmentation_file_path = os.path.join(ground_truth_folder, segmentation_filename)
-        
+    for output_file_path in output_filepaths:
+
+        image_name = output_file_path.split("/")[-1][1:]
+        image_with_extension = image_name.split(".")
+        image_with_extension[1] = "png"
+        image_with_extension[0] += "_Segmentation"
+        ground_truth_image_name = ".".join(image_with_extension)
+        ground_truth_path = ground_truth_folder + "/" + ground_truth_image_name
+
         total_jaccard = 0
         total_items = 0
         total_dice = 0
 
-        if os.path.exists(segmentation_file_path):
-            testing_image = cv2.imread(base_file_path, cv2.IMREAD_GRAYSCALE)
-            segmentation_image = cv2.imread(segmentation_file_path, cv2.IMREAD_GRAYSCALE)
-            total_jaccard += jaccard(testing_image, segmentation_image)
-            total_dice += dice(testing_image, segmentation_image)
+        if os.path.exists(ground_truth_path):
+            testing_image = cv2.imread(output_file_path, cv2.IMREAD_GRAYSCALE)
+            print(testing_image)
+            segmentation_image = cv2.imread(ground_truth_path, cv2.IMREAD_GRAYSCALE)
+            print(segmentation_image)
+            jaccard_score = jaccard(testing_image, segmentation_image)
+            dice_score = dice(testing_image, segmentation_image)
+            total_jaccard += jaccard_score
+            total_dice += dice_score
             total_items += 1
-            print(f"Found corresponding segmentation file for {base_filename}: {segmentation_filename}")
+            print(f"Found corresponding segmentation file for {output_file_path}: {ground_truth_path}")
+            print("Jaccard Score for images: ", jaccard_score)
+            print("Dice Score for images: ", dice_score)
+
         else:
-            print(f"No corresponding segmentation file found for {base_filename}")
+            print(f"No corresponding segmentation file found for {output_file_path}")
 
     print("Dice Score: ", total_dice/total_items)
     print("Jaccard Score: ", total_jaccard/total_items)

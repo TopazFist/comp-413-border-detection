714af35bc7800b2642928394835deb9825a9a7f5
irrundle3 <irrundle3@github.com>
2024-04-11 19:30:46 -0500

    reformatting image display

--- frontend/src/pages/PatientHome.jsx
+++ frontend/src/pages/PatientHome.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Box from '@mui/material/Box';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 const PatientHome = () => {
   const { id } = useParams(); // Extracting physician ID from route parameters
@@ -26,28 +31,40 @@ const PatientHome = () => {
   };
 
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-6">Patient Home</h1>
-      <div className="grid grid-cols-3 gap-8">
-        {patientImages.map(patientImage => (
-          <div key={patientImage._id} className="relative">
-            <label className="absolute top-0 left-0 mt-2 ml-2 bg-white p-2 rounded-lg">
+    <Box sx={{my: 10}}>
+      <h1 className="text-3xl font-bold mb-6">Patient images: {id}</h1>
+      <ImageList cols={3}>
+        <ImageListItem key="upload">
+          <div className="relative flex justify-center items-center">
+            <div style={{width: 300}}onClick={handleUploadClick}>
+              <svg style={{ maxHeight: '200px' }} xmlns="http://www.w3.org/2000/svg" className="h-full w-full mx-auto text-gray-400 hover:text-gray-600" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9 9V5a1 1 0 112 0v4h4a1 1 0 110 2h-4v4a1 1 0 11-2 0v-4H5a1 1 0 110-2h4z" clipRule="evenodd" />
+              </svg>
+            </div>
+          </div>
+        </ImageListItem>
+        {patientImages.map((patientImage) => (
+          <ImageListItem key={patientImage._id} className="border border-gray-300 rounded-lg p-4 cursor-pointer">
+            {/* <label className="absolute top-0 left-0 mt-2 ml-2 bg-white p-2 rounded-lg">
               <input type="checkbox" className="mr-1" checked={patientImage.isPublic} />
               Public
-            </label>
-            <img src={"http://localhost:3001/" + patientImage.s3image} alt={patientImage.s3image} className="w-full h-full object-cover" style={{ maxHeight: '200px' }} />
-          </div>
+            </label> */}
+            <img
+              src={"http://localhost:3001/" + patientImage.s3image} 
+              alt={patientImage.s3image} 
+              className="w-full h-full object-cover" 
+              style={{ maxHeight: '200px' }} 
+            />
+            <ImageListItemBar
+              title={patientImage.s3image}
+              subtitle={patientImage._id}
+              position="below"
+            />
+          </ImageListItem>
         ))}
-        <div className="relative flex justify-center items-center">
-          <div className="w-full h-full max-w-xs max-h-xs border border-gray-300 rounded-lg p-4 cursor-pointer" onClick={handleUploadClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 mx-auto text-gray-400 hover:text-gray-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9 9V5a1 1 0 112 0v4h4a1 1 0 110 2h-4v4a1 1 0 11-2 0v-4H5a1 1 0 110-2h4z" clipRule="evenodd" />
-            </svg>
-          </div>
-        </div>
-      </div>
-    </div>
+      </ImageList>
+    </Box>
   );
-};
+}
 
 export default PatientHome;

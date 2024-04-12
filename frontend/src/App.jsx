fdf897a661925df662533b294e88ca90a1f48463
Amy Zuo <az28@rice.edu>
2024-04-12 15:30:05 -0500

    Added nurse post-login page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // import React from "react";
-import {Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import WelcomePage from './pages/Welcome';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import CreatePatient from "./pages/CreatePatient";
@@ -7,8 +8,7 @@ import CreatePhysician from "./pages/CreatePhysician";
 import PatientHome from "./pages/PatientHome";
 import PhysicianHome from "./pages/PhysicianHome";
 import ImageUpload from "./pages/ImageUpload";
-
-import WelcomePage from './pages/Welcome';
+import NurseHome from "./pages/NurseHome";
 
 import './styles/styles.css';
 import PhysicianLogin from "./pages/loginPhysician";
@@ -20,15 +20,15 @@ const App = () => {
                         <Route path="/" element={<Navigate to="/welcome" />} /> {/* Redirect to welcome page */}
 
                         <Route path="/welcome" element={<WelcomePage />} />
-                        {/* <Route path="/" element={<Navigate to="/login" />} /> Redirect to login page */}
                         <Route path="/patients/login" element={<Login />} />
                         <Route path='/patients/register' element={<Register />} />
                         <Route path='/registerpatient' element={<CreatePatient />} />
-                        <Route path='/physicians/register' element={<CreatePhysician />} />
                         <Route path='/physicians/login' element={<PhysicianLogin />} />
+                        <Route path='/physicians/register' element={<CreatePhysician />} />
                         <Route path='/patients/:id' element={<PatientHome />} />
                         <Route path='/patients/:id/upload' element={<ImageUpload />} />
                         <Route path='/physicians/:id' element={<PhysicianHome />} />
+                        <Route path='/nurses/:id' element={<NurseHome />} />
                 </Routes>
         )
 }

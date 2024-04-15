2e824801bf6c1a6e47f4a6a0e3e8fd91352a8cf0
Amy Zuo <az28@rice.edu>
2024-04-14 23:30:10 -0500

    Updated image upload page.

--- frontend/src/pages/ImageUpload.jsx
+++ frontend/src/pages/ImageUpload.jsx
@@ -2,72 +2,54 @@ import { useState, useRef } from "react";
 import { useParams } from 'react-router-dom'
 import axios from "axios";
 import FormData from "form-data";
-// import { PORT } from "/Users/amyzuo/Downloads/comp-413-border-detection/backend/config.js";
 import "../styles/imageUpload.css";
 
 const ImageUpload = () => {
     const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState("No image chosen");
     const inputRef = useRef(null);
     const { id } = useParams();
 
     const handleButtonClick = () => {
-        console.log("Button is clicked!");
         inputRef.current.click();
     };
 
-    const handleImageUpload = async (event) => {
-        const image = event.target.files[0];
-        if (image) {
-            setFile(image);
-        }
-        
-        const formData = new FormData();
-        formData.append("file", image);
-        console.log(image);
-
-        const response = await axios.post(`http://localhost:3001/upload/` + id , formData);
-        console.log(response);
-        // .then(response => console.log(response))
-        // .catch(error => console.log(error));
-    };
-
-    const handleDrop = (event) => {
-        event.preventDefault();
-        const uploadedFile = event.dataTransfer.files[0];
-        if (uploadedFile) {
-            setFile(uploadedFile);
+    const handleImageSelect = (event) => {
+        const selectedFile = event.target.files[0];
+        if (selectedFile) {
+            setFile(selectedFile);
+            setFileName(selectedFile.name);
         }
     };
 
-    const showFile = () => {
-        if (!file) {
-            return null;
-        }
+    const handleImageUpload = async () => {
+        if (file) {
+            const formData = new FormData();
+            formData.append("file", file);
+            console.log(file);
 
-        const fileType = file.type;
-        const validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-        if (validExtensions.includes(fileType)) {
-            const fileURL = URL.createObjectURL(file);
-            return <img src={fileURL} alt="Uploaded" />;
+            try {
+                const response = await axios.post(`http://localhost:3001/upload/${id}`, formData);
+                console.log("Upload successful: ", response);
+
+                setFile(null);
+                setFileName("No image chosen");
+            } catch (error) {
+                console.error("Error: ", error);
+            }
         } else {
-            alert("This is not a valid image!");
-            return null;
+            alert("No file selected!");
         }
     };
 
     return (
-        <section className="drag-area"
-                 onDrop={handleDrop}>
-            <section className="icon">
-                {/* <i className="cloud-upload"></i> */}
-            </section>
-            <header>
-                {"Drag & Drop Image"}
-            </header>
-            <span>or</span>
-            <button onClick={handleButtonClick}>Browse Image</button>
-            <input ref={inputRef} type="file" hidden onChange={handleImageUpload}></input>
-            {file && showFile()}
+        <section className="drag-area">
+            <div>
+                <button onClick={handleButtonClick}>Choose Image</button>
+                <span>{fileName}</span>
+            </div>
+            <input ref={inputRef} type="file" hidden onChange={handleImageSelect} />
+            <button onClick={handleImageUpload}>Upload Image</button>
         </section>
     );
 }

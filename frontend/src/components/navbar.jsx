fd8ccdfc0dd3985becd9b2793de54717c52482b3
Atishay Jain <atishayericjain@gmail.com>
2024-04-16 14:15:45 -0500

    changed navbar for nurse login

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,33 +7,22 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-// import dermascope_logo from './dermascope_logo.svg';
 import full_logo from './full_logo.svg';
 
-const pages = ['Patient Login', "Physician Login", "Logout"];
-const links = ['/patients/login', '/physicians/login', '/']
+const pages = ['Patient Login', 'Physician Login', 'Nurse Login', 'Logout'];
+const links = ['/patients/login', '/physicians/login', '/nurses/login', '/'];
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
@@ -41,7 +30,12 @@ function Navbar() {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box component="a" href="/">
-            <Box component="img" sx={{ display: { height: 50, xs: 'none', md: 'flex'}, mr: 1 }} src={full_logo} alt="logo"/>
+            <Box
+              component="img"
+              sx={{ display: { height: 50, xs: 'none', md: 'flex' }, mr: 1 }}
+              src={full_logo}
+              alt="logo"
+            />
           </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
@@ -73,7 +67,7 @@ function Navbar() {
               }}
             >
               {pages.map((page, i) => (
-                <MenuItem key={page} onClick = {() => {window.location.href = links[i]}}>
+                <MenuItem key={page} onClick={() => { window.location.href = links[i] }}>
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
@@ -83,43 +77,13 @@ function Navbar() {
             {pages.map((page, i) => (
               <Button
                 key={page}
-                onClick = {() => {window.location.href = links[i]}}
+                onClick={() => { window.location.href = links[i] }}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 {page}
               </Button>
             ))}
           </Box>
-
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>

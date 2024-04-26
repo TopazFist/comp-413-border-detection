508ec94e00b3e9479855f2c3de99d4f6fe869cc9
Amy Zuo <az28@rice.edu>
2024-04-26 03:10:00 -0500

    Added models comments.

--- backend/models/nurseAuthModel.js
+++ backend/models/nurseAuthModel.js
@@ -1,20 +1,29 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+// Model that provides functionalities related to nurse authentication.
+
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema } = mongoose;
 
+// Define the schema for nurse authentication data
 const nurseAuthSchema = new Schema({
+  // Username for nurse authentication
   username: { type: String, required: true },
+  // Password for nurse authentication
   password: { type: String, required: true },
+  // ID of the corresponding nurse
   nurseId: { type: String, required: true }
 });
 
-// Hashing the password before saving
+// Middleware to hash the password before saving
 nurseAuthSchema.pre('save', async function(next) {
   try {
+    // Check if the password has been modified
     if (!this.isModified('password')) {
       return next();
     }
+
+    // Hash the password using bcrypt with a salt factor of 10
     const hashedPassword = await bcrypt.hash(this.password, 10);
     this.password = hashedPassword;
     next();
@@ -23,9 +32,18 @@ nurseAuthSchema.pre('save', async function(next) {
   }
 });
 
-// Method to validate password
+/**
+ * Method to validate the password provided by the user.
+ * 
+ * @param {string} password - The password to be validated.
+ * 
+ * @returns {boolean} A boolean indicating whether the provided password is valid.
+ * 
+ * @throws {Error} If there's an error during password validation.
+ */
 nurseAuthSchema.methods.isValidPassword = async function(password) {
   try {
+    // Compare the provided password with the stored hashed password
     const isValid = await bcrypt.compare(password, this.password);
     return isValid;
   } catch (error) {
@@ -33,4 +51,5 @@ nurseAuthSchema.methods.isValidPassword = async function(password) {
   }
 };
 
+// Create and export the model based on the defined schema
 export const NurseAuth = mongoose.model('NurseAuth', nurseAuthSchema);
--- backend/models/physicianAuthModel.js
+++ backend/models/physicianAuthModel.js
@@ -1,8 +1,11 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+// Model that provides functionalities related to physician authentication.
+
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema } = mongoose;
 
+// Define the schema for physician authentication data
 const physicianAuthSchema = new Schema({
   username: {
     type: String,
@@ -18,12 +21,15 @@ const physicianAuthSchema = new Schema({
   }
 });
 
-// Hashing the password before saving
+// Middleware to hash the password before saving
 physicianAuthSchema.pre('save', async function(next) {
   try {
+    // Check if the password has been modified
     if (!this.isModified('password')) {
       return next();
     }
+
+    // Hash the password using bcrypt with a salt factor of 10
     const hashedPassword = await bcrypt.hash(this.password, 10);
     this.password = hashedPassword;
     next();
@@ -32,16 +38,24 @@ physicianAuthSchema.pre('save', async function(next) {
   }
 });
 
-// Method to validate password
+/**
+ * Method to validate the password provided by the user.
+ * 
+ * @param {string} password - The password to be validated.
+ * 
+ * @returns {boolean} A boolean indicating whether the provided password is valid.
+ * 
+ * @throws {Error} If there's an error during password validation.
+ */
 physicianAuthSchema.methods.isValidPassword = async function(password) {
   try {
-    console.log('Stored hashed password:', this.password);
+    // Compare the provided password with the stored hashed password
     const isValid = await bcrypt.compare(password, this.password);
-    console.log('Generated hashed password:', await bcrypt.hash(password, 10));
     return isValid;
   } catch (error) {
     throw new Error(error);
   }
 };
 
+// Create and export the model based on the defined schema
 export const PhysicianAuth = mongoose.model('PhysicianAuth', physicianAuthSchema);

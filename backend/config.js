d1335d2c76ae40faba89f1f0fa95cdc7d3bb5650
Amy Zuo <az28@rice.edu>
2024-04-26 04:15:35 -0500

    Added backend comments.

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,7 @@
+// Contains constants used in the application.
+
+// Port number for the server
 export const PORT = 3001;
 
-export const mongoDBURL = "mongodb+srv://comp413:comp413@comp413-border-detectio.pf1mqdq.mongodb.net/?retryWrites=true&w=majority&appName=comp413-border-detection"
+// MongoDB connection URL
+export const mongoDBURL = "mongodb+srv://comp413:comp413@comp413-border-detectio.pf1mqdq.mongodb.net/?retryWrites=true&w=majority&appName=comp413-border-detection";
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
+// Serves as the entry point for the application.
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import session from "express-session";
 import { PORT, mongoDBURL } from "./config.js";
-
-import {patientRoutes} from "./routes/patients.js";
-import {physicianRoutes} from "./routes/physicians.js";
-import {authRoutes} from "./routes/auth.js";
+import { patientRoutes } from "./routes/patients.js";
+import { physicianRoutes } from "./routes/physicians.js";
+import { authRoutes } from "./routes/auth.js";
 import { imageRoutes } from "./routes/images.js";
 import { uploadRoutes } from "./routes/upload.js"
 import { nurseRoutes } from "./routes/nurses.js";
@@ -15,66 +16,62 @@ const PRODUCTION = false;
 
 const app = express();
 
-// middleware for parsing request body
+// Middleware for parsing request body
 app.use(express.json());
 
-// middleware for handling cors policy
-// it would be better to customize - do this later if necessary
+// Middleware for handling CORS policy
 const corsOptions = {
-    credentials: true, // Allow credentials (cookies)
+    // Allow credentials (cookies)
+    credentials: true,
     origin: 'http://localhost:5173',
 };
 app.use(cors(corsOptions));
 
+// Session configuration
 const secret = "comp413";
 if (PRODUCTION) {
-    secret = process.env.SECRET // Gets the SECRET environment variable
+    // Use the SECRET environment variable in production
+    secret = process.env.SECRET;
 }
 app.use(session({
-    secret: secret, // for encryption
-    resave: false, // only saves when modified
-    saveUninitialized: false, // avoid saving empty sessions
-    cookie: { maxAge: 1_800_000 } // session lasts 30 mins from inactivity (1.8M milliseconds)
+    // Encryption
+    secret: secret,
+    // Only saves when modified
+    resave: false,
+    // Avoid saving empty sessions
+    saveUninitialized: false,
+    // 30 minutes session duration (1.8M milliseconds)
+    cookie: { maxAge: 1_800_000 }
 }))
+
+// Serve uploaded images statically
 app.use("/image-uploads", express.static("image-uploads"));
 
+// Test route
 app.get('/', (request, response) => {
-        console.log(request);
-        return response.status(234).send("hi!");
+    console.log(request);
+    return response.status(234).send("hi!");
 });
 
 // Debugging middleware to log incoming requests
 app.use((req, res, next) => {
-        console.log(`Received ${req.method} request at ${req.path}`);
-        next();
-    });
-// Routes
-app.use('/patients',patientRoutes)
+    console.log(`Received ${req.method} request at ${req.path}`);
+    next();
+});
 
-app.use('/physicians',physicianRoutes)
+// Routes setup
+app.use('/auth', authRoutes);
+app.use('/patients', patientRoutes);
+app.use('/physicians', physicianRoutes);
+app.use('/nurses', nurseRoutes);
+app.use('/images', imageRoutes);
+app.use('/upload', uploadRoutes);
 
-app.use('/auth', authRoutes)
-
-app.use('/nurses', nurseRoutes)
-
-app.use('/images', imageRoutes)
-
-
-// Connecting the database
-// mongoose.connect(mongoDBURL)
-//         .then(()=> {
-//                 console.log("App connected to database");
-//                 app.listen(PORT, () => {
-//                         console.log(`App is listening to port: ${PORT}`);
-//                 })
-//         })
-//         .catch((error)=>{
-//                 console.log(error);
-//         })
-      
+// Connect to MongoDB database
 mongoose.connect(mongoDBURL)
     .then(() => {
         console.log("App connected to database");
+        // Start the server
         app.listen(PORT, () => {
             console.log(`App is listening to port: ${PORT}`);
         })
@@ -89,5 +86,3 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Internal server error' });
 });
-
-app.use("/upload", uploadRoutes);

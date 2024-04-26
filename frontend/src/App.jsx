a726de69a60299b34da980a9ba16979ccabe91c9
Amy Zuo <az28@rice.edu>
2024-04-26 17:00:25 -0500

    Added frontend comments.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,68 +1,77 @@
-// import React from "react";
-import { Routes, Route, Navigate } from 'react-router-dom'
-import WelcomePage from './pages/Welcome';
+import { Routes, Route, Navigate } from "react-router-dom";
+import WelcomePage from "./pages/Welcome";
+import Unauthorized from "./pages/Unauthorized";
+import Logout from "./pages/Logout";
 import Login from "./pages/Login";
+import PhysicianLogin from "./pages/loginPhysician";
+import NurseLogin from "./pages/loginNurse";
 import Register from "./pages/Register";
 import CreatePhysician from "./pages/CreatePhysician";
-import CreatePatientFromPhysician from "./pages/CreatePatientFromPhysician";
+import NurseRegister from "./pages/CreateNurse";
 import PatientHome from "./pages/PatientHome";
+import PhysicianHome from "./pages/PhysicianHome";
+import NurseHome from "./pages/NurseHome";
 import PatientProfile from "./pages/PatientProfile";
 import PhysicianProfile from "./pages/PhysicianProfile";
-import PhysicianHome from "./pages/PhysicianHome";
 import ViewPatient from "./pages/ViewPatient";
+import NurseViewPatient from "./pages/NurseViewPatient";
+import CreatePatientFromPhysician from "./pages/CreatePatientFromPhysician";
+import AddPatientFromNurse from "./pages/AddPatientFromNurse";
 import ImageUpload from "./pages/ImageUpload";
 import Navbar from "./components/navbar";
 import Box from "@mui/material/Box";
-import NurseRegister from './pages/CreateNurse'; 
-import NurseLogin from './pages/loginNurse';
-import NurseViewPatient from "./pages/NurseViewPatient";
-import AddPatientFromNurse from "./pages/AddPatientFromNurse";
-import Unauthorized from "./pages/Unauthorized";
-import Logout from "./pages/Logout";
+import "./styles/styles.css";
 
-// import PhysicianLogin from "./pages/newLoginPhysician";
-import NurseHome from "./pages/NurseHome";
-
-import './styles/styles.css';
-import PhysicianLogin from "./pages/loginPhysician";
-
+/**
+ * Main component of the application responsible for routing and rendering different pages.
+ */
 const App = () => {
         return (
                 <Box >
                         <Navbar />
+                        {/* Routing setup */}
                         <Routes>
                                 {/* <Route path='/' element={<Login />} /> */}
-                                <Route path="/" element={<Navigate to="/welcome" />} /> {/* Redirect to welcome page */}
+
+                                {/* Redirect to welcome page */}
+                                <Route path="/" element={<Navigate to="/welcome" />} />
 
                                 <Route path="/welcome" element={<WelcomePage />} />
                                 <Route path='/unauthorized' element={<Unauthorized />} />
                                 <Route path="/logout" element={<Logout />} />
 
+                                {/* Login routes */}
                                 <Route path="/patients/login" element={<Login />} />
                                 <Route path='/physicians/login' element={<PhysicianLogin />} />
                                 <Route path='/nurses/login' element={<NurseLogin />} />
 
+                                {/* Registration routes */}
                                 <Route path='/patients/register' element={<Register />} />
                                 <Route path='/physicians/register' element={<CreatePhysician />} />
                                 <Route path='/nurses/register' element={<NurseRegister />} />
 
+                                {/* Home routes */}
                                 <Route path='/patients/:id' element={<PatientHome />} />
                                 <Route path='/physicians/:id' element={<PhysicianHome />} />
                                 <Route path='/nurses/:id' element={<NurseHome />} />
 
+                                {/* Profile routes */}
                                 <Route path='/patients/:id/profile' element={<PatientProfile />} />
                                 <Route path='/physicians/:id/profile' element={<PhysicianProfile />} />
 
+                                {/* View patient routes */}
                                 <Route path='/physicians/patients/:id' element={<ViewPatient />} />
                                 <Route path="/nurses/patients/:id" element={<NurseViewPatient />} />
 
+                                {/* Add patient routes */}
                                 <Route path='/physicians/:id/patients/create' element={<CreatePatientFromPhysician />} />
                                 <Route path="/nurses/:id/patients/add" element={<AddPatientFromNurse />} />
 
+                                {/* Image upload route */}
                                 <Route path='/patients/:id/upload' element={<ImageUpload />} />
                         </Routes>
                 </Box>
         )
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
+/**
+ * Render the root of the React application.
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Wrap the App component with BrowserRouter for routing functionality
   <BrowserRouter>
     <App />
   </BrowserRouter>,
